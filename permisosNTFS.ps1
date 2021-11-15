4ae0ab3dcd63091a01bdda0eab3f1e17f002c250
aluvaz5529 <91330510+aluvaz5529@users.noreply.github.com>
2021-11-15 12:40:40 +0100

    Update permisosNTFS.ps1

--- permisosNTFS.ps1
+++ permisosNTFS.ps1
@@ -1,9 +1,10 @@
+#en ruta poner las variables SU RE SE IN
 #1. Obtenemos la lista acl (permisos NTFS) de la carpeta
 $acl = Get-Acl -Path $ruta
 "1.1 Quitamos la herencia copiando los permisos
 $acl.SetAccessRuleProtection($true,$true)
 $acl | Set-Acl -Path $ruta
-1.2 Eliminamos el grupo Usuarios del dominio (Usuarios)
+#1.2 Eliminamos el grupo Usuarios del dominio (Usuarios)
 $acl.Access | Where-Object {$_.IdentityReference -eq "BUILTIN\Usuarios"} | Foreach-Object {$acl.RemoveAccessRule($_) | Out-Null}
 $acl | Set-Acl $ruta
 #2. Creamos un nuevo FileSystemAccessRule 
@@ -12,3 +13,9 @@ $regla = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule
 $acl.SetAccessRule($regla)
 #4. Aplicar la nueva ACL al archivo o carpeta (Añadir permisos a la carpeta)
 $acl | Set-Acl -Path $ruta
+
+
+#comprobar permisos
+#$acl.Access
+#get-acl $ruta | fl
+
