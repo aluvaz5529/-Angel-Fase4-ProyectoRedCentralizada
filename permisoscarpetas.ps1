5483439c1157cb3ff66a085a4be4a15f1e16cefc
aluvaz5529 <91330510+aluvaz5529@users.noreply.github.com>
2021-11-15 12:16:27 +0100

    Update permisoscarpetas.ps1

--- permisoscarpetas.ps1
+++ permisoscarpetas.ps1
@@ -13,3 +13,19 @@ New-Item -Path $IN -ItemType Directory
 New-SmbShare -Name Publico-RC -Path $SU -FullAccess SMR-GL-Total-DirPublico -ReadAccess SMR-GL-Read-DirPublic `
 -ChangeAccess SMR-GL-Change-DirPublico -ConcurrentUserLimit 28 `
 -Description "Carpeta publico para el acceso de usuarios"
+
+#version 2
+$SU = 'F:\ruta servicios universitarios'
+$RE = 'F:\ruto rectorado'
+$SE = 'F:\ruta secretaria'
+$IN = 'F:\Servicios_universitarios'
+
+#Creamos la carpeta que hemos añadido en la variable $ruta 
+New-Item -Path $SU -ItemType Directory
+New-Item -Path $RE -ItemType Directory
+New-Item -Path $SE -ItemType Directory
+New-Item -Path $IN -ItemType Directory
+
+New-SmbShare -Name Publico-RC -Path $SU -WriteAccess Cs_GL_WServicios_U -ReadAccess Cs_GL_RServicios_U`
+-ChangeAccess Cs_GG_Servicios_U -ConcurrentUserLimit 28 `
+-Description "Carpeta publico para el acceso de usuarios"
