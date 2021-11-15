1623d24e0788a9fc61e28066fe794cca8eda4845
aluvaz5529 <91330510+aluvaz5529@users.noreply.github.com>
2021-11-15 12:36:54 +0100

    Create permisosNTFS.ps1

---
+++ permisosNTFS.ps1
@@ -0,0 +1,14 @@
+#1. Obtenemos la lista acl (permisos NTFS) de la carpeta
+$acl = Get-Acl -Path $ruta
+"1.1 Quitamos la herencia copiando los permisos
+$acl.SetAccessRuleProtection($true,$true)
+$acl | Set-Acl -Path $ruta
+1.2 Eliminamos el grupo Usuarios del dominio (Usuarios)
+$acl.Access | Where-Object {$_.IdentityReference -eq "BUILTIN\Usuarios"} | Foreach-Object {$acl.RemoveAccessRule($_) | Out-Null}
+$acl | Set-Acl $ruta
+#2. Creamos un nuevo FileSystemAccessRule 
+$regla = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule("SMR_GL_R_DirPublico", "Read", "Allow") 
+#3.Agregar la nueva regla
+$acl.SetAccessRule($regla)
+#4. Aplicar la nueva ACL al archivo o carpeta (Añadir permisos a la carpeta)
+$acl | Set-Acl -Path $ruta
