1cb992128cb7c1abc420ae7703d262d2172bf840
aluvaz5529 <91330510+aluvaz5529@users.noreply.github.com>
2021-11-15 12:12:55 +0100

    Create permisoscarpetas.ps1

---
+++ permisoscarpetas.ps1
@@ -0,0 +1,15 @@
+#permisos para carpteass del segundo disco (globales)
+$SU = 'F:\ruta servicios universitarios'
+$RE = 'F:\ruto rectorado'
+$SE = 'F:\ruta secretaria'
+$IN = 'F:\Servicios_universitarios'
+
+#Creamos la carpeta que hemos añadido en la variable $ruta 
+New-Item -Path $SU -ItemType Directory
+New-Item -Path $RE -ItemType Directory
+New-Item -Path $SE -ItemType Directory
+New-Item -Path $IN -ItemType Directory
+
+New-SmbShare -Name Publico-RC -Path $SU -FullAccess SMR-GL-Total-DirPublico -ReadAccess SMR-GL-Read-DirPublic `
+-ChangeAccess SMR-GL-Change-DirPublico -ConcurrentUserLimit 28 `
+-Description "Carpeta publico para el acceso de usuarios"
