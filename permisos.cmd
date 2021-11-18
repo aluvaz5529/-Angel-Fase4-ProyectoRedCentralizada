f312789e45e571d43df66fcccabd45fd54133cb2
aluvaz5529 <91330510+aluvaz5529@users.noreply.github.com>
2021-11-18 13:57:02 +0100

    Update permisos.cmd

--- permisos.cmd
+++ permisos.cmd
@@ -6,9 +6,9 @@ if NOT EXIST F:\departamentos_cs\Rectorado mkdir F:\departamentos_cs\Rectorado
 if NOT EXIST F:\departamentos_cs\Secretaria mkdir F:\departamentos_cs\Secretaria
 if NOT EXIST F:\departamentos_cs\Servicios mkdir F:\departamentos_cs\Informatica
 REM Compartimos en red con 'Todos' las carpetas, con los permisos NTFS filtraremos los accesos 
-net share D_Servicios_U=F:\departamentos_cs\Servicios_U  /GRANT:Cs_GG_Rectorado,full
-net share D_Servicios_U=F:\departamentos_cs\Servicios_U /GRANT:Cs_GL_RServiciosU,read
-net share D_Servicios_U=F:\departamentos_cs\Servicios_U /GRANT:Cs_GL_WServiciosU,write
+net share D_Servicios_U=F:\departamentos_cs\Servicios_U  /GRANT:Cs_GG_Servicios_U,full
+net share D_Servicios_U=F:\departamentos_cs\Servicios_U /GRANT:Cs_GL_RServicios_U,read
+net share D_Servicios_U=F:\departamentos_cs\Servicios_U /GRANT:Cs_GL_WServicios_U,write
 net share D_Rectorado=F:\departamentos_cs\Rectorado /GRANT:Cs_GG_Rectorado,full
 net share D_Rectorado=F:\departamentos_cs\Rectorado /GRANT:Cs_GL_WRectorado,write
 net share D_Rectorado=F:\departamentos_cs\Rectorado /GRANT:Cs_GL_RRectorado,read
