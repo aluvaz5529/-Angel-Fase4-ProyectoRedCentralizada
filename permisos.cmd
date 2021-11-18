93ebe3b1254d6a55e4db524140cde9f34bed2cb4
aluvaz5529 <91330510+aluvaz5529@users.noreply.github.com>
2021-11-18 15:32:27 +0100

    Update permisos.cmd

--- permisos.cmd
+++ permisos.cmd
@@ -6,17 +6,13 @@ if NOT EXIST F:\departamentos_cs\Rectorado mkdir F:\departamentos_cs\Rectorado
 if NOT EXIST F:\departamentos_cs\Secretaria mkdir F:\departamentos_cs\Secretaria
 if NOT EXIST F:\departamentos_cs\Servicios mkdir F:\departamentos_cs\Informatica
 REM Compartimos en red con 'Todos' las carpetas, con los permisos NTFS filtraremos los accesos 
-net share D_Servicios_U=F:\departamentos_cs\Servicios_U  /GRANT:Cs_GG_Servicios_U,full
-net share D_Servicios_U=F:\departamentos_cs\Servicios_U  /GRANT:Cs_GL_RServicios_U,read
-net share D_Servicios_U=F:\departamentos_cs\Servicios_U  /GRANT:Cs_GL_WServicios_U,write
-net share D_Rectorado=F:\departamentos_cs\Rectorado /GRANT:Cs_GG_Rectorado,full
-net share D_Rectorado=F:\departamentos_cs\Rectorado /GRANT:Cs_GL_WRectorado,write
+net share D_Servicios_U=F:\departamentos_cs\Servicios_U /GRANT:Cs_GL_RServicios_U,read /GRANT:Cs_GL_WServicios_U,change
+REM net share D_Servicios_U=F:\departamentos_cs\Servicios_U /GRANT:Cs_GL_WServicios_U,change
+net share D_Rectorado=F:\departamentos_cs\Rectorado /GRANT:Cs_GL_WRectorado,change
 net share D_Rectorado=F:\departamentos_cs\Rectorado /GRANT:Cs_GL_RRectorado,read
-net share D_Secretaria=F:\departamentos_cs\Secretaria /GRANT:Cs_GG_Secretaria,full
-net share D_Secretaria=F:\departamentos_cs\Secretaria /GRANT:Cs_GL_WSecretaria,write
+net share D_Secretaria=F:\departamentos_cs\Secretaria /GRANT:Cs_GL_WSecretaria,change
 net share D_Secretaria=F:\departamentos_cs\Secretaria /GRANT:Cs_GL_RSecretaria,read
-net share D_Informatica=F:\departamentos_cs\Informatica /GRANT:Cs_GG_Informatica,full
-net share D_Informatica=F:\departamentos_cs\Informatica /GRANT:Cs_GL_WInformatica,write
+net share D_Informatica=F:\departamentos_cs\Informatica /GRANT:Cs_GL_WInformatica,change
 net share D_Informatica=F:\departamentos_cs\Informatica /GRANT:Cs_GL_RInformatica,read
 REM de aqui hacia arriba no tocar nada que funciona
 REM 
