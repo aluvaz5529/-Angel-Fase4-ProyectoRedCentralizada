ee441764f4c4702e93c144e17a2d8af37e3f41ec
aluvaz5529 <91330510+aluvaz5529@users.noreply.github.com>
2021-11-24 22:13:46 +0100

    Update apagar.cmd

--- información/apagar.cmd
+++ información/apagar.cmd
@@ -1,4 +1,4 @@
 @echo off
-REM se gurada en C:\Windows\SYSVOL\sysvol\castellon.upv.es\scripts
+REM se guarda en C:\Windows\SYSVOL\sysvol\castellon.upv.es\scripts
 echo El servidor %computername% se ha apagado el %date% a las %time% >> C:\log\paradas.log 
 pause
