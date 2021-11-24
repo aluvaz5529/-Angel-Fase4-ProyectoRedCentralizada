d1aec25dfc29da1e61a7dab3f22e6ee4b539dfd8
aluvaz5529 <91330510+aluvaz5529@users.noreply.github.com>
2021-11-24 22:19:10 +0100

    Update usuarios.cmd

--- información/usuarios.cmd
+++ información/usuarios.cmd
@@ -1,6 +1,6 @@
 @echo off
 REM se guarda en C:\Windows\SYSVOL\sysvol\castellon.upv.es\scripts
-IF NOT EXIST X: net use X: \\CAMPUS-CASTELLON\Almacenamiento
+IF NOT EXIST C: net use C: \\CAMPUS-CASTELLON\Almacenamiento
 msg %username% Recuerda que los datos guardados en los discos locales pueden perderse, 
 utiliza las unidades R: o S:
 echo El usuario %username% ha iniciado sesión el %date% a las %time% en el equipo
