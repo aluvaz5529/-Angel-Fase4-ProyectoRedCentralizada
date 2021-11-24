1325d607953a6bb7a4608d9a230af3ea33fc2627
aluvaz5529 <91330510+aluvaz5529@users.noreply.github.com>
2021-11-24 22:15:18 +0100

    Update usuarios.cmd

--- información/usuarios.cmd
+++ información/usuarios.cmd
@@ -3,5 +3,5 @@ REM se guarda en C:\Windows\SYSVOL\sysvol\castellon.upv.es\scripts
 IF NOT EXIST X: net use X: \\CAMPUS-CASTELLON\Almacenamiento
 msg %username% Recuerda que los datos guardados en los discos locales pueden perderse, 
 utiliza las unidades R: o S:
-echo El usuario %username% ha iniciado sesión el %date% a las %time% en el equipo actual
+echo El usuario %username% ha iniciado sesión el %date% a las %time% en el equipo
 %computername% >> \\CAMPUS-CASTELLON\log\conexiones.log
