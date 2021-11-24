49fcb0586238b4d4d6fe356cd584259f241146f7
aluvaz5529 <91330510+aluvaz5529@users.noreply.github.com>
2021-11-24 17:44:25 +0100

    Update usuarios.cmd

--- información/usuarios.cmd
+++ información/usuarios.cmd
@@ -1,4 +1,5 @@
-@echo off 
+@echo off
+REM se guarda en C:\Windows\SYSVOL\sysvol\castellon.upv.es\scripts
 IF NOT EXIST X: net use X: \\CAMPUS-CASTELLON\Almacenamiento
 msg %username% Recuerda que los datos guardados en los discos locales pueden perderse, 
 utiliza las unidades R: o S:
